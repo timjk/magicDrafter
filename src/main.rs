ed537ab5ad85c3da5d4b98a10d17ba06a720ffa7
timjk <timfjk@gmail.com>
2019-10-08 13:24:51 +1300

    Handle unfound scryfall cards

--- src/main.rs
+++ src/main.rs
@@ -22,17 +22,21 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
         .author("timjk <timfjk@gmail.com>")
         .about("Helps when drafting in magicArena")
         .subcommand(SubCommand::with_name("pull")
-            .about("Pulls down the latest card details"))
-        .subcommand(SubCommand::with_name("init")
-            .about("Initializes the card db"))
+            .about("Pulls down the latest card definitions for the user"))
+        .subcommand(SubCommand::with_name("update")
+            .about("Updates the card definitions for magic_drafter"))
         .get_matches();
 
     if let Some(_matches) = matches.subcommand_matches("pull") {
-        get_ranks()?;
+        // This will pull the latest definitions for the user, stored in a google doc & insert them into their
+        // local sqlite DB
+        fetch_card_ranks()?;
         return Ok(())
     }
 
-    if let Some(_matches) = matches.subcommand_matches("init") {
+    if let Some(_matches) = matches.subcommand_matches("update") {
+        // This will pull the latest definitions from the various sites and update the arena drafter google doc
+        // for now it updates a local sqlite DB
         init_db()?;
         return Ok(())
     }
@@ -77,9 +81,8 @@ struct ScryfallCard {
     set_name: String,
 }
 
-fn pull(id: u32) -> Result<ScryfallCard, Error> {
-    let res: ScryfallCard = reqwest::get(&format!("https://api.scryfall.com/cards/arena/{}", id)).unwrap().json().unwrap();
-    Ok(res)
+fn fetch_card_details(id: u32) -> Result<ScryfallCard, reqwest::Error> {
+    reqwest::get(&format!("https://api.scryfall.com/cards/arena/{}", id))?.json()
 }
 
 fn init_db() -> rusqlite::Result<()> {
@@ -92,25 +95,25 @@ fn init_db() -> rusqlite::Result<()> {
             name TEXT NOT NULL,
             scryfallId TEXT NOT NULL,
             cardSet TEXT NOT NULL,
-            cardRank TEXT NOT NULL
+            cardRank TEXT
         )",
         NO_PARAMS)?;
 
-    let cards = get_cards().unwrap();
+    let cards = fetch_arena_cards().unwrap();
     let mut stmt = conn.prepare("SELECT id FROM card")?;
     let existing_cards: HashSet<_> = stmt
         .query_map::<u32, _, _>(NO_PARAMS, |row| row.get(0))?
         .map(|x| x.unwrap())
         .collect();
 
-    let ranks = get_ranks().unwrap();
+    let ranks = fetch_card_ranks().unwrap();
 
     insert_card_defs(&conn, cards.difference(&existing_cards).cloned().collect(), &ranks).unwrap();
     println!("done.");
     Ok(())
 }
 
-fn get_cards() -> Result<HashSet<u32>, Error> {
+fn fetch_arena_cards() -> Result<HashSet<u32>, Error> {
     let res = reqwest::get("https://raw.githubusercontent.com/mtgatracker/node-mtga/master/mtga/m20.js").unwrap().text().unwrap();
 
     let re = Regex::new(r"mtgaID: (\d+), ").unwrap();
@@ -119,28 +122,34 @@ fn get_cards() -> Result<HashSet<u32>, Error> {
     Ok(records)
 }
 
-fn get_ranks() -> Result<HashMap<String, String>, Error> {
+fn fetch_card_ranks() -> Result<HashMap<String, String>, Error> {
     let res = reqwest::get("https://docs.google.com/spreadsheets/d/1BAPtQv4U9KUAtVzkccJlPS8cb0s_uOcGEDORip5uaQg/gviz/tq?headers=0&sheet=Staging%20Sheet&tq=select+A,D")
         .unwrap().text().unwrap();
 
     let re = Regex::new(r#"c[^v]+v.{3}([^"]+)".{8}([^"]+)"#).unwrap();
 
     let card_ranks: HashMap<String, String> = re.captures_iter(&res)
-        .map(|x| (x[1].to_owned(), x[2].to_owned()))
+        .map(|x| (str::replace(&x[1].to_owned(), "\\u0027", "'"), x[2].to_owned()))
         .collect();
-    
+
     Ok(card_ranks)
 }
 
-// This should return a vector of cards but am sneaking in a db update while we wait to make next request
+// This should return a vector of cards but haven't figured a way to do multi-insert with rusqlite
 fn insert_card_defs(conn: &Connection, card_ids: HashSet<u32>, card_ranks: &HashMap<String, String>) -> Result<(), Error> {
     for id in card_ids {
-        let card = pull(id).unwrap();
+        let card = match fetch_card_details(id) {
+            Ok(x) => x,
+            _ => {
+                println!("Unable to retrieve card details with id {}, skipping...", id);
+                continue;
+            }
+        };
         let card_rank = get_closest_match(&card.name, card_ranks);
         conn.execute(
             "INSERT INTO card (id, name, scryfallId, cardSet, cardRank)
              VALUES (?1, ?2, ?3, ?4, ?5)",
-            &[&card.arena_id as &ToSql, &card.name, &card.id, &card.set_name, &card_rank],
+            &[&card.arena_id as &dyn ToSql, &card.name, &card.id, &card.set_name, &card_rank],
         ).unwrap();
         println!("{:?}{:?}", card, card_rank);
         thread::sleep(time::Duration::from_secs(1)); // be a good citizen
@@ -159,8 +168,3 @@ fn score_card_name<'a>(acc: (i64, &'a str), x: (&String, &'a String), card_name:
     let max = max(acc.0, match fuzzy_match(&card_name, x.0) { Some(y) => y, None => 0 } );
     if max > acc.0 { (max, x.1) } else { acc }
 }
-
-
-// Community set reviews https://docs.google.com/spreadsheets/d/1BAPtQv4U9KUAtVzkccJlPS8cb0s_uOcGEDORip5uaQg/gviz/tq?headers=2&range=A1%3AZ&sheet=Table%20Source&tqx=reqId%3A1;responseHandler%3A__JsonpRequest_cb__.getResponse1_
-// card-name & rating https://docs.google.com/spreadsheets/d/1BAPtQv4U9KUAtVzkccJlPS8cb0s_uOcGEDORip5uaQg/gviz/tq?headers=0&sheet=Staging%20Sheet&tq=select+A,D 
-// https://www.mtgcommunityreview.com/core-set-2020
